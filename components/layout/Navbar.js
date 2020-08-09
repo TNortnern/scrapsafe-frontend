71544b67eeb2546cc20db546e66fc55a25b73f6d
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-09 15:37:30 -0400

    Update Navbar.js

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -8,7 +8,7 @@ import HamburgerReact from './HamburgerReact';
 const Navbar = () => {
   const { user, setUser } = useContext(AppContext);
   return (
-    <nav className='flex flex-col items-center sm:flex-row justify-between px-6 py-2 mx-4 relative'>
+    <nav className='flex flex-col items-center sm:flex-row justify-between px-6 py-2 mx-4 relative z-30'>
       <NavLogo />
       <HamburgerReact />
       <ul id='navigation' className='hidden sm:flex sm:space-x-5 text-center'>
