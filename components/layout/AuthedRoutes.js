345373568bb6f310820f3ccccd8544660ec67049
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-09 16:03:46 -0400

    fix navbar

--- components/layout/AuthedRoutes.js
+++ components/layout/AuthedRoutes.js
@@ -1,39 +1,42 @@
-import Link from "next/link";
-import { remove } from "js-cookie";
-import Router from "next/router";
+import Link from 'next/link';
+import { remove } from 'js-cookie';
+import Router from 'next/router';
 
-const AuthedRoutes = ({setUser}) => {
-    return (
-      <>
-        <li>
-          <Link href="/gridview">
-            <a className="hover:text-green-custom">Grid View</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a className="hover:text-green-custom"> Upload An Image</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/settings">
-            <a className="hover:text-green-custom">Settings</a>
-          </Link>
-        </li>
-        <li>
-          <span
-            onClick={() => {
-              setUser(null);
-              remove("user_token");
-              Router.push("/login");
-            }}
-            className="hover:text-green-custom cursor-pointer"
-          >
-            Log Out
-          </span>
-        </li>
-      </>
-    );
-}
+const AuthedRoutes = ({ setUser }) => {
+  return (
+    <>
+      <li className='border-b border-gray-500 sm:border-none py-2'>
+        <Link href='/gridview'>
+          <a className='hover:text-green-custom'>Grid View</a>
+        </Link>
+      </li>
+      <li className='border-b border-gray-500 sm:border-none py-2'>
+        <Link href='/'>
+          <a className='hover:text-green-custom sm:border-none'>
+            {' '}
+            Upload An Image
+          </a>
+        </Link>
+      </li>
+      <li className='border-b border-gray-500 sm:border-none py-2'>
+        <Link href='/settings'>
+          <a className='hover:text-green-custom'>Settings</a>
+        </Link>
+      </li>
+      <li className='border-b border-gray-500 sm:border-none py-2'>
+        <span
+          onClick={() => {
+            setUser(null);
+            remove('user_token');
+            Router.push('/login');
+          }}
+          className='hover:text-green-custom cursor-pointer'
+        >
+          Log Out
+        </span>
+      </li>
+    </>
+  );
+};
 
 export default AuthedRoutes;
--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -8,10 +8,13 @@ import HamburgerReact from './HamburgerReact';
 const Navbar = () => {
   const { user, setUser } = useContext(AppContext);
   return (
-    <nav className='flex flex-col items-center sm:flex-row justify-between px-6 py-2 mx-4 relative z-30'>
+    <nav className='flex flex-col items-center sm:flex-row justify-between px-6 py-2 mx-4 relative z-30 mb-4 sm:mb-0'>
       <NavLogo />
       <HamburgerReact />
-      <ul id='navigation' className='hidden sm:flex sm:space-x-5 text-center'>
+      <ul
+        id='navigation'
+        className='hidden sm:flex  sm:space-x-5 text-center bg-white z-30 w-full sm:w-auto'
+      >
         {!user ? <UnauthedRoutes /> : <AuthedRoutes setUser={setUser} />}
       </ul>
     </nav>
