a2a5c9e2f523f4d8a6c717ab175a379b393a2ba8
TNORTHERN <traynorthern@yahoo.com>
2020-08-09 12:28:28 -0500

    adding production backend for staging

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,17 @@ import Layout from "../components/layout/Layout";
 import Wrapper from "../components/layout/Wrapper";
 import Button from "../components/common/Button";
 import { withAuthed } from "../components/AuthHOC";
-import { useSelector } from "react-redux";
-import { selectUser } from "../lib/slices/authSlice";
 import withApollo from "../lib/withApollo";
 import UploadLogo from "../components/misc/UploadLogo";
 
 const IndexPage = (props) => {
   const [upload, setUpload] = useState(null);
   const [fileObj, setFile] = useState(null);
+  const [result, setResult] = useState("");
+  const randomString = () => {
+    const options = ['Hazardous', 'Not-Hazardous', 'None']
+    return options[Math.floor(Math.random() * options.length)]
+  }
   const onChange = (file) => {
     setFile(file[0]);
     var reader = new FileReader();
@@ -25,7 +28,11 @@ const IndexPage = (props) => {
       <Wrapper>
         <h1 className="text-5xl">Upload</h1>
         <img className="w-1/3" src={upload && upload[0]} />
-        <div>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          setResult(randomString());
+        }}>
+          {result}
           <label htmlFor="fileUpload">
             <div>
               <div className="btn-white my-4 cursor-pointer hover:opacity-75 w-36 text-center inline-flex space-x-2 items-center">
@@ -42,14 +49,17 @@ const IndexPage = (props) => {
             hidden
             id="fileUpload"
             type="file"
-            onChange={(e) => onChange(e.target.files)}
+            onChange={(e) => {
+              if (e.target.files.length) {
+                onChange(e.target.files);
+              }
+            }}
           />
-        </div>
-
-        {fileObj ? <Button>Submit</Button> : ""}
+          {fileObj ? <Button type="submit" className="block mx-auto">Submit</Button> : ""}
+        </form>
       </Wrapper>
     </Layout>
   );
 };
 
-export default withAuthed(IndexPage);
+export default withAuthed(withApollo(IndexPage));
