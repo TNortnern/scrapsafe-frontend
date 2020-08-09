a3aea0ff42d8098a94bfe1e75e29bb665d647df6
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-09 15:00:04 -0400

    Gridview responive

--- pages/gridview.js
+++ pages/gridview.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Layout from "../components/layout/Layout";
-import Wrapper from "../components/layout/Wrapper";
+import React, { useState, useEffect } from 'react';
+import Layout from '../components/layout/Layout';
+import Wrapper from '../components/layout/Wrapper';
 
 const gridview = () => {
   const [images, setImages] = useState([]);
   const generateFakes = () => {
-    const imageChoice = ["/fake1.png", "/fake2.png"];
+    const imageChoice = ['/fake1.png', '/fake2.png'];
     const count = 15;
     for (let i = 0; i < count; i++) {
       const randomImageIndex = Math.floor(Math.random() * imageChoice.length);
-      console.log("randomImageIndex", randomImageIndex);
+      console.log('randomImageIndex', randomImageIndex);
       setImages([...images, imageChoice[randomImageIndex]]);
     }
   };
@@ -18,15 +18,41 @@ const gridview = () => {
   }, []);
   return (
     <Layout>
-      <Wrapper>
-        <div className="flex space-y-2 flex-wrap">
-          <img src="/fake1.png" alt="fake" />
-          <img src="/fake2.png" alt="fake" />
-          <img src="/fake1.png" alt="fake" />
-          <img src="/fake2.png" alt="fake" />
-          <img src="/fake1.png" alt="fake" />
+      <div className='w-11/12 lg:w-11/12  mx-auto px-3'>
+        <div className='flex flex-wrap justify-center'>
+          {console.log('images', images)}
+          <img
+            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
+            src='/fake1.png'
+            alt='fake'
+          />
+          <img
+            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
+            src='/fake2.png'
+            alt='fake'
+          />
+          <img
+            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
+            src='/fake1.png'
+            alt='fake'
+          />
+          <img
+            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
+            src='/fake1.png'
+            alt='fake'
+          />
+          <img
+            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
+            src='/fake2.png'
+            alt='fake'
+          />
+          <img
+            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
+            src='/fake1.png'
+            alt='fake'
+          />
         </div>
-      </Wrapper>
+      </div>
     </Layout>
   );
 };
