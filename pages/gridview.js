39dca40e5c7184a8ab1bbefc09a1e591a73ba23a
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-09 16:30:51 -0400

    Gridview text under img

--- pages/gridview.js
+++ pages/gridview.js
@@ -19,38 +19,70 @@ const gridview = () => {
   return (
     <Layout>
       <div className='w-11/12 lg:w-11/12  mx-auto px-3'>
-        <div className='flex flex-wrap justify-center'>
+        <div className='flex flex-wrap justify-center text-xs'>
           {console.log('images', images)}
-          <img
-            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
-            src='/fake1.png'
-            alt='fake'
-          />
-          <img
-            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
-            src='/fake2.png'
-            alt='fake'
-          />
-          <img
-            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
-            src='/fake1.png'
-            alt='fake'
-          />
-          <img
-            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
-            src='/fake1.png'
-            alt='fake'
-          />
-          <img
-            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
-            src='/fake2.png'
-            alt='fake'
-          />
-          <img
-            className=' max-w-xs sm:w-4/12 w-1/2 lg:w-8/12'
-            src='/fake1.png'
-            alt='fake'
-          />
+
+          {/* Start of first div */}
+          <div className='max-w-xs sm:w-4/12 w-1/2 lg:w-8/12 my-2'>
+            <img className=' ' src='/fake1.png' alt='fake' />
+            <p>
+              <span className='font-bold'>Jack Norman - </span>
+              Lorem ipsum dolor sit amet.
+            </p>
+            <p>
+              <span className='font-bold'>Type: </span> Lorem ipsum dolor sit
+              amet.
+            </p>
+            <p>
+              <span className='font-bold'>Hazmat:</span> Mercury
+            </p>
+          </div>
+          {/* End of first Div */}
+
+          <div className='max-w-xs sm:w-4/12 w-1/2 lg:w-8/12 my-2'>
+            <img className=' ' src='/fake1.png' alt='fake' />
+            <p>
+              <span className='font-bold'>Jack Norman - </span>
+              Lorem ipsum dolor sit amet.
+            </p>
+            <p>
+              <span className='font-bold'>Type: </span> Lorem ipsum dolor sit
+              amet.
+            </p>
+            <p>
+              <span className='font-bold'>Hazmat:</span> Mercury
+            </p>
+          </div>
+
+          <div className='max-w-xs sm:w-4/12 w-1/2 lg:w-8/12 my-2'>
+            <img className=' ' src='/fake1.png' alt='fake' />
+            <p>
+              <span className='font-bold'>Jack Norman - </span>
+              Lorem ipsum dolor sit amet.
+            </p>
+            <p>
+              <span className='font-bold'>Type: </span> Lorem ipsum dolor sit
+              amet.
+            </p>
+            <p>
+              <span className='font-bold'>Hazmat:</span> Mercury
+            </p>
+          </div>
+
+          <div className='max-w-xs sm:w-4/12 w-1/2 lg:w-8/12 my-2'>
+            <img className=' ' src='/fake1.png' alt='fake' />
+            <p>
+              <span className='font-bold'>Jack Norman - </span>
+              Lorem ipsum dolor sit amet.
+            </p>
+            <p>
+              <span className='font-bold'>Type: </span> Lorem ipsum dolor sit
+              amet.
+            </p>
+            <p>
+              <span className='font-bold'>Hazmat:</span> Mercury
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
